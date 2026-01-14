bfb214a7c6fe92ec08a0abb8990b7a1194eb0279
Cesar <ce181760@ucf.edu>
2026-01-14 16:36:20 -0500

    Fix unsafe any usage in fib.ts

--- src/fib.ts
+++ src/fib.ts
@@ -1,12 +1,20 @@
 // util function that computes the fibonacci numbers
-export default function fibonacci(n) {
-  if (n < 0) {
-    return -1;
-  } else if (n == 0) {
-    return 0;
-  } else if (n == 1) {
-    return 1;
+export function fibonacci(n: number): number[] {
+  if (!Number.isInteger(n) || n < 1) {
+    throw new Error('Input must be a positive integer');
   }
 
-  return fibonacci(n - 1) + fibonacci(n - 2);
+  const fibNumbers: number[] = [];
+
+  for (let i = 0; i < n; i++) {
+    if (i === 0) {
+      fibNumbers.push(0);
+    } else if (i === 1) {
+      fibNumbers.push(1);
+    } else {
+      fibNumbers.push(fibNumbers[i - 1] + fibNumbers[i - 2]);
+    }
+  }
+
+  return fibNumbers;
 }
